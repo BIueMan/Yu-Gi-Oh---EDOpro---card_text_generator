a4da7712527d2d6c94900e7576e28c5cdca31aa7
Dan Ben David <bddan013@gmail.com>
2023-10-27 16:53:03 +0300

    code now read the lua script with the card text

--- read_data/main_functions.py
+++ read_data/main_functions.py
@@ -32,10 +32,12 @@ def get_card_mapping_data(file_path: str)->Dict[str, Dict[str, int]]:
 class cards_data():
     def __init__(self, cards_data_path: str = 'data/cards_text/datas.csv',
                        cards_text_path: str = 'data/cards_text/texts.csv',
-                       cards_data_mapping_path: str = 'data/type_mapping/constant.lua'):
+                       cards_data_mapping_path: str = 'data/type_mapping/constant.lua',
+                       cards_script_path: str = 'data/EDOpro_script/official/'):
         self.cards_data = pd.read_csv(cards_data_path, delimiter='|')
         self.cards_text = pd.read_csv(cards_text_path, delimiter='|')
         self.cards_data_mapping = get_card_mapping_data(cards_data_mapping_path)
+        self.cards_script_path = cards_script_path
     
     def get_id_list(self)->np.array:
         return self.cards_data.get('id')
@@ -58,7 +60,7 @@ class cards_data():
         type_map = self.cards_data_mapping['TYPE']
         types_list = [key for key in type_map.keys() if type_map[key] & data.get('type') and key != 'ALL']
         
-        def card_layout():
+        def card_layout() -> str:
             # add card type
             card_layout = ""
             card_layout += "card_name: {}\n".format(text.iloc[1])
@@ -84,7 +86,18 @@ class cards_data():
                 
         return card_layout()
     
+    def get_card_scripe(self, card_id: int) -> str:
+        script_location = self.cards_script_path + 'c' + str(card_id) + '.lua'
+        try:
+            with open(script_location, 'r', encoding='utf-8') as file:
+                return '\n'.join(file.read().split('\n')[1:])
+        except IOError:
+            print("Couldn't open card script {}".format(script_location))
+        
+    
 if __name__ == '__main__':
     cd = cards_data()
     card_layout = cd.read_card(0)
+    card_script = cd.get_card_scripe(cd.get_id_list()[0])
     print(card_layout)
+    print(card_script)
